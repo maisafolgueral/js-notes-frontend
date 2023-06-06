a69fec1befcee3deac7c83d797446fea118376cf
Maisa <maisa.folgueral@gmail.com>
2023-06-06 17:50:01 -0300

    Added home screen

---
+++ src/screens/home/index.js
@@ -0,0 +1,44 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { Fragment } from "react";
+import presentationImage from "../../assets/images/presentation.png";
+import Header from "../../components/header";
+import "../../styles/home.scss";
+
+function Home() {
+  return (
+    <Fragment>
+      <Header />
+
+      <section className="section home">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-5">
+              <h2 className="title is-2 has-text-white">
+                Create notes easily and access when you want on the cloud
+              </h2>
+              <h5 className="title is-5 has-text-light">
+                Lorem ipsum, or lipsum as it is sometimes known, is dummy text
+                used in laying out print, graphic or web designs.
+                <br />
+                <br />
+                Lorem ipsum, or lipsum as it is sometimes known, is dummy text
+                used in laying out print.
+              </h5>
+              <a className="button is-outlined is-white is-large">
+                <strong>Register for free Now</strong>
+              </a>
+            </div>
+
+            <div className="column is-6 is-offset-1">
+              <img src={presentationImage} alt="Presentation" />
+            </div>
+
+            <div className="column">{/* Conteúdo da coluna */}</div>
+          </div>
+        </div>
+      </section>
+    </Fragment>
+  );
+}
+
+export default Home;
