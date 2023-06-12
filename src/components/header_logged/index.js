59a01f8f272885eaf8a2d9b7e7ab2243f4ed6f2a
Maisa <maisa.folgueral@gmail.com>
2023-06-12 16:58:01 -0300

    Added header logged mobile

--- src/components/header_logged/index.js
+++ src/components/header_logged/index.js
@@ -28,47 +28,68 @@ function HeaderLogged(props) {
 
   return (
     <nav color="custom-purple" className="navbar navbar-logged img-header">
-      <div className="navbar-brand">
-        <Link to="/notes">
-          <img src={LogoImage} alt="Logo" />
-        </Link>
-        <button
-          className={`navbar-burger burger`}
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbar-menu"
-        ></button>
-      </div>
+      <div className="container">
+        <div className="navbar-brand">
+          <Link to="/notes">
+            <img src={LogoImage} alt="Logo" />
+          </Link>
+          <div
+            className={`burguer-logged navbar-burger burger ${isOpen ? "is-active" : ""}`}
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbar-menu"
+            onClick={handleMenuToggle}
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </div>
+        </div>
 
-      <div id="navbarBasicExample" className="navbar-menu">
-        <div align="right" className="navbar-item navbar-end">
-          <div className="navbar-item">
-            <div className={`dropdown is-right ${isOpen ? "is-active" : ""}`}>
-              <div className="dropdown-trigger">
-                <Button
-                  className="button"
-                  color="white"
-                  onClick={handleMenuToggle}
-                  outlined
+        <div
+          id="navbarBasicExample"
+          className={`navbar-menu ${isOpen ? "is-active" : ""}`}
+        >
+          <div align="right" className="navbar-item navbar-end">
+            <div className="navbar-item">
+              <div className={`dropdown is-right ${isOpen ? "is-active" : ""}`}>
+                <div className="dropdown-trigger">
+                  <Button
+                    className="button"
+                    color="white"
+                    onClick={handleMenuToggle}
+                    outlined
+                  >
+                    <span>Exemplo ▼</span>
+                  </Button>
+                </div>
+                <div
+                  className={`dropdown-menu ${isOpen ? "is-active" : ""}`}
+                  id="dropdown-menu3"
+                  role="menu"
                 >
-                  <span>Exemplo ▼</span>
-                </Button>
-              </div>
-              <div class="dropdown-menu" id="dropdown-menu3" role="menu">
-                <div className="dropdown-content is-centered">
-                  <div className="dropdown-item">
-                    <Link
-                      to="/users/edit"
-                      className="button is-white has-text-custom-purple is-fullwidth"
-                    >
-                      User Edit
-                    </Link>
-                  </div>
-                  <div className="dropdown-divider" />
-                  <div className="dropdown-item">
-                    <a href="#" onClick={logOut} className="button is-white has-text-custom-purple is-fullwidth">
-                      LogOut
-                    </a>
+                  <div className="dropdown-content is-centered">
+                    <div className="dropdown-item">
+                      <Link
+                        to="/users/edit"
+                        className="button is-white has-text-custom-purple is-fullwidth"
+                      >
+                        User Edit
+                      </Link>
+                    </div>
+                    <div className="dropdown-divider" />
+                    <div className="dropdown-item">
+                      <a
+                        href="#"
+                        onClick={() => {
+                          handleMenuToggle();
+                          logOut();
+                        }}
+                        className="button is-white has-text-custom-purple is-fullwidth"
+                      >
+                        LogOut
+                      </a>
+                    </div>
                   </div>
                 </div>
               </div>
