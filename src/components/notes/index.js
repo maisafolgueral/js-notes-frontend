fc2db83f2aa6b8376f5ee42f6fdcf6c1039e8918
Maisa <maisa.folgueral@gmail.com>
2023-06-13 16:02:04 -0300

    Added notes list

--- src/components/notes/index.js
+++ src/components/notes/index.js
@@ -1,11 +1,39 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import "../../styles/notes.scss";
 import { slide as Menu } from "react-burger-menu";
+import List from "../notes/list";
+import NoteService from "../../services/note";
 
 function Notes(props) {
+  const [notes, setNotes] = useState([]);
+  const [current_note, setCurrentNote] = useState({
+    title: "",
+    body: "",
+    id: "",
+  });
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  async function fetchNotes() {
+    const response = await NoteService.index();
+    if (response.data.length >= 1) {
+      setNotes(response.data.reverse());
+      setCurrentNote(response.data[0]);
+    }
+  }
+
+  const selectNote = (id) => {
+    const note = notes.find((note) => {
+      return note._id === id;
+    });
+    setCurrentNote(note);
+  };
+
   return (
     <Fragment>
-      <div className="columns notes" id="notes">
+      <div className="notes" id="notes">
         <Menu
           pageWrapId={"notes-editor"}
           isOpen={props.isOpen}
@@ -20,7 +48,13 @@ function Notes(props) {
               <div className="column is-10 is-offset-1">Search...</div>
             </div>
           </div>
-          <p>List...</p>
+
+          <List
+            notes={notes}
+            selectNote={selectNote}
+            current_note={current_note}
+          />
+          <br/>
         </Menu>
 
         <div className="column is-12 notes-editor" id="notes-editor">
---
+++ src/components/notes/list/index.js
@@ -0,0 +1,41 @@
+import React, { Fragment } from "react";
+import Moment from "moment";
+import "../../../styles/notes.scss";
+
+function ListNotes(props) {
+  return (
+    <Fragment>
+      <div className="columns" breakpoint="mobile">
+        <div className="column is-6 is-offset-1">
+          <h6 class="title is-6">{props.notes.length} Notes</h6>
+        </div>
+      </div>
+      <ul className="notes-list">
+        {props.notes.map((item, key) => (
+          <li
+            key={key}
+            onClick={() => props.selectNote(item._id)}
+            className={item === props.current_note ? "selected" : ""}
+          >
+            <h6 class="subtitle is-6">
+              {item.title.replace(/(<([^>]+)>)/gi, "").substring(0, 15)}
+            </h6>
+            <h6 class="subtitle is-6" spaced={false}>
+              {item.body.replace(/(<([^>]+)>)/gi, "").substring(0, 30)}
+            </h6>
+            <div className="columns" breakpoint="mobile">
+              <div className="column is-10">
+                <span class="tag is-dark">
+                  {Moment(item.created_at).format("DD/MM")}
+                </span>
+              </div>
+            </div>
+            <br/>
+          </li>
+        ))}
+      </ul>
+    </Fragment>
+  );
+}
+
+export default ListNotes;
