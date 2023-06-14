9a4b31e8ba72a366adc9ec439bea823a82db440d
Maisa <maisa.folgueral@gmail.com>
2023-06-14 18:23:26 -0300

    Added user edit screen

--- src/screens/users/edit/index.js
+++ src/screens/users/edit/index.js
@@ -1,5 +1,39 @@
 import React, { Fragment } from "react";
+import HeaderLogged from "../../../components/header_logged";
 
-const UsersEditScreen = () => <Fragment>Hello World</Fragment>;
+const UsersEditScreen = () => (
+  <Fragment>
+    <HeaderLogged />
+    <section className="section is-medium users">
+      <div class="container">
+        <div className="columns is-centered users-edit">
+          <div className="column is-4">
+            <h5 class="title is-5 has-text-grey has-text-left">
+              Informações Pessoais
+            </h5>
+            <div class="card">
+              <div class="card-content">Users Edit Form...</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="columns is-centered users-edit">
+          <div className="column is-4">
+            <h5 class="title is-5 has-text-grey has-text-left">Password</h5>
+            <div class="card">
+              <div class="card-content">Users Edit Password Form...</div>
+            </div>
+          </div>
+        </div>
+        <div className="columns is-centered">
+          <div className="column is-4 has-text-right">
+            Users Delete Button...
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </Fragment>
+);
 
 export default UsersEditScreen;
